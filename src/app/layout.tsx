33000aecedc38dbe2a34ac8862facad3f3f7fd0d
davkooz <kuzmicz.dawid@gmail.com>
2025-02-13 14:52:12 +0100

    Remove unused Oxanium font import to streamline layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Onest } from 'next/font/google';
-import { Oxanium } from 'next/font/google';
 import "./globals.css";
 
 const geistSans = Geist({
